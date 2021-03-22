1c470b115ac4a821141a200e001c00e9b1eafeca
Vighnesh1209-IoT <vighneshmindia@gmail.com>
2021-03-22 17:09:06 +0530

    Update fetch_data_from_ESP.php

---
+++ fetch_data_from_ESP.php
@@ -0,0 +1,71 @@
+<?php
+
+//Server details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "smarttrolley";
+
+
+$key = "77289";
+$api_key_value = "dd";
+
+//Getting value form Nodemcu
+$api_key = $RFID_ID = "";
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+  $api_key = test_input($_POST["api_key"]);
+  if($api_key == $api_key_value){
+    $RFID_ID = test_input($_POST["RFIDtag"]);
+  }
+}
+
+//database Connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//fetching data from default_products for one specific RFIDtag
+$sql = "SELECT RFID, NAME, TYPE, PRICE FROM default_products WHERE RFID = $key";
+
+if ($result = $conn->query($sql)) {
+    while ($row = $result->fetch_assoc()) {
+        $sensor = $row["RFID"];
+        $location = $row["NAME"];
+        $value1 = $row["TYPE"];
+        $value2 = $row["PRICE"];
+
+        //send the fetched data to coustumer product(Check if its already present and then add)
+        $query1 = "SELECT RFID FROM customer_products WHERE RFID = $key";
+        $result1 = mysqli_query($conn,$query1);
+        if ($result1) {
+          if (mysqli_num_rows($result1) > 0) {
+            echo 'found!';
+          } else {
+            echo 'not found';
+            $sql2 = "INSERT INTO customer_products(RFID, NAME, TYPE, PRICE) VALUES ('" . $sensor . "', '" . $location . "', '" . $value1 . "', '" . $value2 . "')";
+            if ($conn->query($sql2) === TRUE) {
+                  echo "New record created successfully";
+              }
+              else {
+                  echo "Error: " . $sql2 . "<br>" . $conn->error;
+              }
+          }
+        } else {
+          echo 'Error';
+        }
+      }
+    $result->free();
+
+}
+
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$conn->close();
+?>
