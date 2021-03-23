d01c77cb5ba30ddad759dd52ec4a03679ce346d9
Vighnesh1209-IoT <vighneshmindia@gmail.com>
2021-03-23 13:25:06 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,16 +4,25 @@ include "dbConn.php"; // Using database connection file here
 
 $id = $_GET['id']; // get id through query string
 
+if(isset($_POST['Confirm'])){
 $del = mysqli_query($conn,"delete from customer_products where RFID = '$id'"); // delete query
-
 if($del)
 {
     mysqli_close($conn); // Close connection
-    header("location:display.php"); // redirects to all records page
+    header("location:home.php"); // redirects to all records page
     exit;
 }
 else
 {
     echo "Error deleting record"; // display error message if not delete
 }
+}
+elseif (isset($_POST['Goback'])) {
+  header("location:home.php"); // redirects to all records page
+  exit;
+}
 ?>
+<form method="POST">
+             <input type="submit" name="Confirm" value="Confirm">
+             <input type="submit" name="Goback" value="Back">
+</form>
