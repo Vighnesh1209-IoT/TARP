dc646c4344d762727795ce98f2b2c25b46de57d6
Vighnesh1209-IoT <vighneshmindia@gmail.com>
2021-03-23 12:23:21 +0530

    Update display.php

--- display.php
+++ display.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-h1 {
-  color: Chartreuse;
-  text-align: center;
-}
-h3{
-  color: DarkOrange;
-  text-align: center;
-}
+  <style>
+  table {
+    border-collapse: collapse;
+    width: 50%;
+	  margin: auto;
+  }
+
+  th, td {
+    text-align: left;
+    padding: 8px;
+  }
+  tr:nth-child(even){background-color: #f2f2f2}
+  th {
+    background-color: #000;
+    color: white;
+  }
 </style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 </head>
-<title> Welcome to Dubai Supermarket </title>
+
 <body>
   <style>
   body {background-image: url("background.jpg");}
@@ -22,5 +31,30 @@ h3{
   <?php
   include 'C:\xampp\htdocs\TARP\TARP\Costumer_Table.php'
   ?>
+  <script type="text/javascript">
+    $(document).ready(function(){
+      function showData()
+      {
+        $.ajax({
+          url: 'C:\xampp\htdocs\TARP\TARP\fetch_data.php',
+          type: 'POST',
+          data: {action : 'showProcess'},
+          dataType: 'html',
+          success: function(result)
+          {
+            $('#show').html(result);
+          },
+          error: function()
+          {
+            alert("Failed to show the logs");
+          }
+        })
+      }
+      setInterval(function(){ showData(); }, 1000);
+    });
+
+  </script>
 </body>
+
+
 </html>
