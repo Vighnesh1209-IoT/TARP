95e43ffa4755180e7e2716adf0a4177b380bc3fc
AbineshVel <81150700+AbineshVel@users.noreply.github.com>
2021-03-24 17:42:51 +0530

    Update Customer_Information.php

--- Customer_Information.php
+++ Customer_Information.php
@@ -22,13 +22,3 @@ if ($conn->query($sql2) === TRUE)
       }
 
 ?>
-
-
-<form  method = "POST">
-  Name: <input type="text" name="name" required /> <br>
-  EmailID: <input type="text" name="EmailID" required /> <br>
-  PhoneNo: <input type ="text" name="PhoneNo" required /> <br>
-  Address: <input type ="text" name="Address" /> <br>
-  Gender:  <input type ="text" name="Gender" /> <br>
-  <input type="submit" name="Submit" value="Submit">
-</form>
