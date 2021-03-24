8c130725e0bd864b1b1b191eb52eac2c49aad43f
AbineshVel <81150700+AbineshVel@users.noreply.github.com>
2021-03-24 16:09:24 +0530

    Update clear_db.php

--- clear_db.php
+++ clear_db.php
@@ -1,7 +1,35 @@
+<?php
 
-include "dbConn.php
+include "dbConn.php";
 
- //main usage -> mysqli_query($con,'TRUNCATE TABLE customer_products);
+if(isset($_POST['Submit'])) // when click on Update button
+{
+    $UID = $_POST['userID'];
+    $PID = $_POST['password'];
+    if(($_POST['userID'] == "Admin") && ($_POST['password'] == "1234")){
+    $trun = mysqli_query($conn,'TRUNCATE TABLE customer_products');
 
+    if($trun)
+    {
+        mysqli_close($conn); // Close connection
+        header("location:cart.php"); // redirects to all records page
+        exit;
+    }
+    else
+    {
+        echo mysqli_error();
+    }
 
- if(isset($_POST['Admin']))
+  }
+  else {
+    echo "Incorrect Username or Password! ";
+  }
+}
+
+?>
+
+<form  method="POST">
+    Name: <input type="text" name="userID" /> <br>
+    Pass: <input type="text" name="password" /> <br>
+    <input type="submit" name="Submit" value="Submit">
+</form>
