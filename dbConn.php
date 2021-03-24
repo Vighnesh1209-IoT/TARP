b23f89fe5c139d615de54a4e21ba67ac6fe938eb
Vighnesh1209-IoT <vighneshmindia@gmail.com>
2021-03-24 19:29:17 +0530

    Update dbConn.php

--- dbConn.php
+++ dbConn.php
@@ -1,8 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "smarttrolley";
+//$username = "root";
+//$password = "";
+//$dbname = "smarttrolley";
+//$servername = "localhost";
+
+
+$username = "pc9gdJUd4g";
+$password = "iHlzW9PAfa";
+$dbname = "pc9gdJUd4g";
+$servername = "remotemysql.com";
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
