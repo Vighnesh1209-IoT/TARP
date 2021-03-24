3bdd6265032d945ff4460c6c1b50b2839542725e
AbineshVel <81150700+AbineshVel@users.noreply.github.com>
2021-03-24 17:42:57 +0530

    Create info.php

---
+++ info.php
@@ -0,0 +1,10 @@
+
+
+<form action = "Customer_Information.php" method = "POST">
+  Name: <input type="text" name="name" required /> <br>
+  EmailID: <input type="text" name="EmailID" required /> <br>
+  PhoneNo: <input type ="text" name="PhoneNo" required /> <br>
+  Address: <input type ="text" name="Address" /> <br>
+  Gender:  <input type ="text" name="Gender" /> <br>
+  <input type="submit" name="Submit" value="Submit">
+</form>
