f2a7f744311452703d7ff2cf588af7842f5469d1
AbineshVel <81150700+AbineshVel@users.noreply.github.com>
2021-03-26 18:24:14 +0530

    Update Customer_Information.php

--- Customer_Information.php
+++ Customer_Information.php
@@ -1,14 +1,14 @@
 <?php
 
 include "dbConn.php";
-$name = $_POST["name"];
-$emailid = $_POST["EmailID"];
-$mobno = $_POST["PhoneNo"];
-$address = $_POST["Address"];
-$gender = $_POST["Gender"];
+$name = $_POST["Name"];
+$mobno = $_POST["phone-number"];
+$emailid = $_POST["email-id"];
 
 
-$sql2 = "INSERT INTO customer_information1(NAME, EMAILID, PHONENO, ADDRESS , GENDER) VALUES ('" . $name . "', '" . $emailid . "', '" . $mobno . "', '" . $address . "','" .$gender ."')";
+$sql2 = "INSERT INTO customer_information1('NAME', 'EMAILID', 'PHONENO') VALUES ('" . $name . "', '" . $emailid . "', '" . $mobno . "')";
+//$edit = mysqli_query($conn,"INSERT INTO customer_information1(NAME, EMAIL ID, PHONENO) VALUES ('" . $name . "', '" . $emailid . "', '" . $mobno . "')");
+
 if ($conn->query($sql2) === TRUE)
 {
     echo "New record created successfully";
@@ -20,5 +20,19 @@ if ($conn->query($sql2) === TRUE)
    else {
       echo "Error: " . $sql2 . "<br>" . $conn->error;
       }
+
+
+//if($edit)
+//{
+//    mysqli_close($conn); // Close connection
+//    header("location:cart.php"); // redirects to all records page
+//    exit;
+//}
+/*else
+{
+    echo mysqli_error();
+}
+*/
+
 
 ?>
